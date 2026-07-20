e36512d936e4e28e096ae8b7b4cb8f72d6f8b31e
Neeschal1 <neeschalpok04@gmail.com>
2026-07-20 14:54:36 +0545

    md: responsive statistics ui to fir for all type of screens

--- client/src/pages/landing/statistics.jsx
+++ client/src/pages/landing/statistics.jsx
@@ -3,12 +3,16 @@ import StatsCard from "../../components/statscard";
 
 const Statistics = () => {
   return (
-    <div className="grid grid-cols-4 gap-5 p-10 bg-white">
-      <StatsCard end={5000} suffix="+" title="Students Trained" />
-      <StatsCard end={1000} suffix="+" title="Job Placement" />
-      <StatsCard end={50} suffix="+" title="Corporate Clients" />
-      <StatsCard end={10} suffix="+" title="Professional Courses" />
-    </div>
+    <section className="bg-white py-4 sm:-py-20 lg:py-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-8">
+          <StatsCard end={5000} suffix="+" title="Students Trained" />
+          <StatsCard end={1000} suffix="+" title="Job Placement" />
+          <StatsCard end={50} suffix="+" title="Corporate Clients" />
+          <StatsCard end={10} suffix="+" title="Professional Courses" />
+        </div>
+      </div>
+    </section>
   );
 };
 
